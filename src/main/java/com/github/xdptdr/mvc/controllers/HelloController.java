db67c3f2e6c45706c195a9b20889031875d76a12
User <User@foobar>
2017-05-12 00:21:54 +0200

    Hello from InternalResourceViewResolver

--- src/main/java/com/github/xdptdr/mvc/controllers/HelloController.java
+++ src/main/java/com/github/xdptdr/mvc/controllers/HelloController.java
@@ -1,18 +1,24 @@
 package com.github.xdptdr.mvc.controllers;
 
-import javax.xml.parsers.ParserConfigurationException;
-
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	public ModelAndView hello() throws ParserConfigurationException {
-		ModelAndView mav = new ModelAndView("hello");
+	@RequestMapping(value = "/helloInternal", method = RequestMethod.GET)
+	public ModelAndView helloInternal() {
+		ModelAndView mav = new ModelAndView("helloInternal");
 		return mav;
+	}
+
+	@RequestMapping(value = "/foo/helloInternal/bar", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
+	@ResponseBody
+	public String fooHelloInternalBar() {
+		return "Hello from internal";
 	}
 }
