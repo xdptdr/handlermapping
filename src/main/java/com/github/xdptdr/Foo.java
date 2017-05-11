2451e9507e7ac3884de8ea536f12fb9eda93a2a6
User <User@foobar>
2017-05-11 21:06:05 +0200

    progress

--- src/main/java/com/github/xdptdr/Foo.java
+++ src/main/java/com/github/xdptdr/Foo.java
@@ -2,7 +2,6 @@ package com.github.xdptdr;
 
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.BeanNameViewResolver;
-import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.ResourceBundleViewResolver;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
@@ -12,35 +11,24 @@ import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 import org.springframework.web.servlet.view.groovy.GroovyMarkupViewResolver;
 import org.springframework.web.servlet.view.jasperreports.JasperReportsViewResolver;
 import org.springframework.web.servlet.view.script.ScriptTemplateViewResolver;
-import org.springframework.web.servlet.view.tiles2.TilesViewResolver;
-import org.springframework.web.servlet.view.velocity.VelocityLayoutViewResolver;
-import org.springframework.web.servlet.view.velocity.VelocityViewResolver;
-import org.springframework.web.servlet.view.xslt.XsltViewResolver;
 
 public class Foo {
 	public void foo() {
-		
+
 		FreeMarkerViewResolver.class.getName();
-		
+
 		GroovyMarkupViewResolver.class.getName();
-		
+
 		JasperReportsViewResolver.class.getName();
 
-		XsltViewResolver.class.getName();
-		
 		ViewResolver.class.getName();
 		ResourceBundleViewResolver.class.getName();
 		UrlBasedViewResolver.class.getName();
-		VelocityViewResolver.class.getName();
-		VelocityLayoutViewResolver.class.getName();
 		InternalResourceViewResolver.class.getName();
 		ScriptTemplateViewResolver.class.getName();
-		TilesViewResolver.class.getName();
-		org.springframework.web.servlet.view.tiles3.TilesViewResolver.class.getName();
-		
+
 		XmlViewResolver.class.getName();
 		BeanNameViewResolver.class.getName();
-		ContentNegotiatingViewResolver.class.getName();
 		ViewResolverComposite.class.getName();
 
 	}
