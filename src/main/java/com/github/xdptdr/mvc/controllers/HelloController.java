3c239b859565a4affaad820d1b04cb9677eaae63
xdptdr <xavierdpt@gmail.com>
2017-05-12 14:02:45 +0200

    url consistency

--- src/main/java/com/github/xdptdr/mvc/controllers/HelloController.java
+++ src/main/java/com/github/xdptdr/mvc/controllers/HelloController.java
@@ -8,14 +8,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloController {
 
-	@RequestMapping(value = "/dummy1", method = RequestMethod.GET)
-	public ModelAndView dummy1() {
+	@RequestMapping(value = "/hello1", method = RequestMethod.GET)
+	public ModelAndView hello1() {
 		ModelAndView mav = new ModelAndView("dummy1");
 		return mav;
 	}
 
-	@RequestMapping(value = "/dummy2", method = RequestMethod.GET)
-	public ModelAndView dummy2() {
+	@RequestMapping(value = "/hello2", method = RequestMethod.GET)
+	public ModelAndView hello2() {
 		ModelAndView mav = new ModelAndView("dummy2");
 		return mav;
 	}
