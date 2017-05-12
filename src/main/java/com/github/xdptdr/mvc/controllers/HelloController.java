b5d18db25fa9b15ccc5f97404d2530f396e3da19
xdptdr <xavierdpt@gmail.com>
2017-05-12 13:49:27 +0200

    url consistency + fix missing file

--- src/main/java/com/github/xdptdr/mvc/controllers/HelloController.java
+++ src/main/java/com/github/xdptdr/mvc/controllers/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HelloController {
 
-	@RequestMapping(value = "/helloGroovy", method = RequestMethod.GET)
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String helloGroovy() {
 		return "helloGroovy.tpl";
 	}
