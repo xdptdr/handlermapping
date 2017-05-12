4d3e25d8ecd0502053f67c65a504768be4b64bb2
xdptdr <xavierdpt@gmail.com>
2017-05-12 10:04:32 +0200

    progress

--- src/main/java/com/github/xdptdr/Foo.java
+++ src/main/java/com/github/xdptdr/Foo.java
@@ -1,7 +1,5 @@
 package com.github.xdptdr;
 
-import org.springframework.web.servlet.view.BeanNameViewResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.ViewResolverComposite;
 import org.springframework.web.servlet.view.XmlViewResolver;
 import org.springframework.web.servlet.view.script.ScriptTemplateViewResolver;
@@ -9,11 +7,9 @@ import org.springframework.web.servlet.view.script.ScriptTemplateViewResolver;
 public class Foo {
 	public void foo() {
 
-		InternalResourceViewResolver.class.getName();
 		ScriptTemplateViewResolver.class.getName();
 
 		XmlViewResolver.class.getName();
-		BeanNameViewResolver.class.getName();
 		ViewResolverComposite.class.getName();
 
 	}
