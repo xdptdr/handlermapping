76b18c4524c1cec9a2477380510f4eaa937b0dde
xdptdr <xavierdpt@gmail.com>
2017-05-17 15:32:53 +0200

    .

--- war/src/main/java/com/github/xdptdr/jca/JCAServlet.java
+++ war/src/main/java/com/github/xdptdr/jca/JCAServlet.java
@@ -14,6 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/jcaservlet")
 public class JCAServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
