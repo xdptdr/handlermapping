0442e4b8d495ff048b649ec3508c85f0ded26e76
xdptdr <xavierdpt@gmail.com>
2017-05-29 15:50:18 +0200

    .

--- mbwar/src/main/java/com/github/xdptdr/mbwar/servlet/JDBCBeanServlet.java
+++ mbwar/src/main/java/com/github/xdptdr/mbwar/servlet/JDBCBeanServlet.java
@@ -7,6 +7,7 @@ import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 
 import javax.ejb.EJB;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,50 +26,89 @@ public class JDBCBeanServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-		PrintWriter pw = resp.getWriter();
-
-		String[] booleanMethods = { "isReadOnly", "allProceduresAreCallable", "allTablesAreSelectable",
-				"autoCommitFailureClosesAllResultSets", "dataDefinitionCausesTransactionCommit",
-				"dataDefinitionIgnoredInTransactions", "doesMaxRowSizeIncludeBlobs", "generatedKeyAlwaysReturned",
-				"isCatalogAtStart", "locatorsUpdateCopy", "nullPlusNonNullIsNull", "nullsAreSortedAtEnd",
-				"nullsAreSortedAtStart", "nullsAreSortedHigh", "nullsAreSortedLow", "storesLowerCaseIdentifiers",
-				"storesLowerCaseQuotedIdentifiers", "storesMixedCaseIdentifiers", "storesMixedCaseQuotedIdentifiers",
-				"storesUpperCaseIdentifiers", "storesUpperCaseQuotedIdentifiers", "supportsANSI92EntryLevelSQL",
-				"supportsANSI92FullSQL", "supportsANSI92IntermediateSQL", "supportsAlterTableWithAddColumn",
-				"supportsAlterTableWithDropColumn", "supportsBatchUpdates", "supportsCatalogsInDataManipulation",
-				"supportsCatalogsInIndexDefinitions", "supportsCatalogsInPrivilegeDefinitions",
-				"supportsCatalogsInProcedureCalls", "supportsCatalogsInTableDefinitions", "supportsColumnAliasing",
-				"supportsConvert", "supportsCoreSQLGrammar", "supportsCorrelatedSubqueries",
-				"supportsDataDefinitionAndDataManipulationTransactions", "supportsDataManipulationTransactionsOnly",
-				"supportsDifferentTableCorrelationNames", "supportsExpressionsInOrderBy", "supportsExtendedSQLGrammar",
-				"supportsFullOuterJoins", "supportsGetGeneratedKeys", "supportsGroupBy", "supportsGroupByBeyondSelect",
-				"supportsGroupByUnrelated", "supportsIntegrityEnhancementFacility", "supportsLikeEscapeClause",
-				"supportsLimitedOuterJoins", "supportsMinimumSQLGrammar", "supportsMixedCaseIdentifiers",
-				"supportsMixedCaseQuotedIdentifiers", "supportsMultipleOpenResults", "supportsMultipleResultSets",
-				"supportsMultipleTransactions", "supportsNamedParameters", "supportsNonNullableColumns",
-				"supportsOpenCursorsAcrossCommit", "supportsOpenCursorsAcrossRollback",
-				"supportsOpenStatementsAcrossCommit", "supportsOpenStatementsAcrossRollback",
-				"supportsOrderByUnrelated", "supportsOuterJoins", "supportsPositionedDelete",
-				"supportsPositionedUpdate", "supportsRefCursors", "supportsSavepoints",
-				"supportsSchemasInDataManipulation", "supportsSchemasInIndexDefinitions",
-				"supportsSchemasInPrivilegeDefinitions", "supportsSchemasInProcedureCalls",
-				"supportsSchemasInTableDefinitions", "supportsSelectForUpdate", "supportsStatementPooling",
-				"supportsStoredFunctionsUsingCallSyntax", "supportsStoredProcedures", "supportsSubqueriesInComparisons",
-				"supportsSubqueriesInExists", "supportsSubqueriesInIns", "supportsSubqueriesInQuantifieds",
-				"supportsTableCorrelationNames", "supportsTransactions", "supportsUnion", "supportsUnionAll",
-				"usesLocalFilePerTable", "usesLocalFiles" };
-
 		try {
 			DatabaseMetaData md = bean.getDatabaseMetaData();
-			for (String booleanMethod : booleanMethods) {
-				Object v = DatabaseMetaData.class.getMethod(booleanMethod).invoke(md);
-				pw.println(booleanMethod + " : " + v);
-			}
-
-		} catch (SQLException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
-				| NoSuchMethodException | SecurityException e) {
+			req.setAttribute("md", md);
+			req.getRequestDispatcher("/WEB-INF/jsp/jdbc.jsp").forward(req, resp);
+		} catch (ServletException | SQLException e) {
 			throw new RuntimeException(e);
 		}
+
+		// PrintWriter pw = resp.getWriter();
+		//
+		// String[] booleanMethods = { "isReadOnly", "allProceduresAreCallable",
+		// "allTablesAreSelectable",
+		// "autoCommitFailureClosesAllResultSets",
+		// "dataDefinitionCausesTransactionCommit",
+		// "dataDefinitionIgnoredInTransactions", "doesMaxRowSizeIncludeBlobs",
+		// "generatedKeyAlwaysReturned",
+		// "isCatalogAtStart", "locatorsUpdateCopy", "nullPlusNonNullIsNull",
+		// "nullsAreSortedAtEnd",
+		// "nullsAreSortedAtStart", "nullsAreSortedHigh", "nullsAreSortedLow",
+		// "storesLowerCaseIdentifiers",
+		// "storesLowerCaseQuotedIdentifiers", "storesMixedCaseIdentifiers",
+		// "storesMixedCaseQuotedIdentifiers",
+		// "storesUpperCaseIdentifiers", "storesUpperCaseQuotedIdentifiers",
+		// "supportsANSI92EntryLevelSQL",
+		// "supportsANSI92FullSQL", "supportsANSI92IntermediateSQL",
+		// "supportsAlterTableWithAddColumn",
+		// "supportsAlterTableWithDropColumn", "supportsBatchUpdates",
+		// "supportsCatalogsInDataManipulation",
+		// "supportsCatalogsInIndexDefinitions",
+		// "supportsCatalogsInPrivilegeDefinitions",
+		// "supportsCatalogsInProcedureCalls",
+		// "supportsCatalogsInTableDefinitions", "supportsColumnAliasing",
+		// "supportsConvert", "supportsCoreSQLGrammar",
+		// "supportsCorrelatedSubqueries",
+		// "supportsDataDefinitionAndDataManipulationTransactions",
+		// "supportsDataManipulationTransactionsOnly",
+		// "supportsDifferentTableCorrelationNames",
+		// "supportsExpressionsInOrderBy", "supportsExtendedSQLGrammar",
+		// "supportsFullOuterJoins", "supportsGetGeneratedKeys",
+		// "supportsGroupBy", "supportsGroupByBeyondSelect",
+		// "supportsGroupByUnrelated", "supportsIntegrityEnhancementFacility",
+		// "supportsLikeEscapeClause",
+		// "supportsLimitedOuterJoins", "supportsMinimumSQLGrammar",
+		// "supportsMixedCaseIdentifiers",
+		// "supportsMixedCaseQuotedIdentifiers", "supportsMultipleOpenResults",
+		// "supportsMultipleResultSets",
+		// "supportsMultipleTransactions", "supportsNamedParameters",
+		// "supportsNonNullableColumns",
+		// "supportsOpenCursorsAcrossCommit",
+		// "supportsOpenCursorsAcrossRollback",
+		// "supportsOpenStatementsAcrossCommit",
+		// "supportsOpenStatementsAcrossRollback",
+		// "supportsOrderByUnrelated", "supportsOuterJoins",
+		// "supportsPositionedDelete",
+		// "supportsPositionedUpdate", "supportsRefCursors",
+		// "supportsSavepoints",
+		// "supportsSchemasInDataManipulation",
+		// "supportsSchemasInIndexDefinitions",
+		// "supportsSchemasInPrivilegeDefinitions",
+		// "supportsSchemasInProcedureCalls",
+		// "supportsSchemasInTableDefinitions", "supportsSelectForUpdate",
+		// "supportsStatementPooling",
+		// "supportsStoredFunctionsUsingCallSyntax", "supportsStoredProcedures",
+		// "supportsSubqueriesInComparisons",
+		// "supportsSubqueriesInExists", "supportsSubqueriesInIns",
+		// "supportsSubqueriesInQuantifieds",
+		// "supportsTableCorrelationNames", "supportsTransactions",
+		// "supportsUnion", "supportsUnionAll",
+		// "usesLocalFilePerTable", "usesLocalFiles" };
+		//
+		// try {
+		// DatabaseMetaData md = bean.getDatabaseMetaData();
+		// for (String booleanMethod : booleanMethods) {
+		// Object v =
+		// DatabaseMetaData.class.getMethod(booleanMethod).invoke(md);
+		// pw.println(booleanMethod + " : " + v);
+		// }
+		//
+		// } catch (SQLException | IllegalAccessException |
+		// IllegalArgumentException | InvocationTargetException
+		// | NoSuchMethodException | SecurityException e) {
+		// throw new RuntimeException(e);
+		// }
 
 	}
 }
