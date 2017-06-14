516cb6ee03354de8ab6b11b424f62a879f772836
xdptdr <xavierdpt@gmail.com>
2017-06-14 12:00:50 +0200

    .

---
+++ mbwar/src/main/java/com/github/xdptdr/mbwar/jaxrs/kol/KolEndpoint.java
@@ -0,0 +1,29 @@
+package com.github.xdptdr.mbwar.jaxrs.kol;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.EntityTag;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@Path("/kol")
+public class KolEndpoint {
+
+	@GET
+	@Path("/get")
+	public Response get(@Context Request request) {
+
+		EntityTag eTag = new EntityTag("toto", true);
+		ResponseBuilder rb = request.evaluatePreconditions(eTag);
+		if (rb != null) {
+			return rb.build();
+		} else {
+			return Response.status(Status.FORBIDDEN).build();
+		}
+	}
+}
