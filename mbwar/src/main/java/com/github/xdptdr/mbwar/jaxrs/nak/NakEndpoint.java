159ca02bbbd5eb820c953a25c0ce3fcf3d5143e9
User <User@foobar>
2017-06-21 18:03:54 +0200

    .

--- mbwar/src/main/java/com/github/xdptdr/mbwar/jaxrs/nak/NakEndpoint.java
+++ mbwar/src/main/java/com/github/xdptdr/mbwar/jaxrs/nak/NakEndpoint.java
@@ -10,10 +10,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.PathSegment;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
 @Provider
