75b64f81671d1e1dc5653643fb8c7f4d0310b049
xdptdr <xavierdpt@gmail.com>
2017-06-23 12:30:26 +0200

    .

---
+++ mbwar/src/main/java/com/github/xdptdr/mbwar/dom/notes/Notes.java
@@ -0,0 +1,18 @@
+package com.github.xdptdr.mbwar.dom.notes;
+
+import com.github.xdptdr.mbwar.jaxrs.notes.N;
+
+@SuppressWarnings("deprecation")
+public class Notes {
+	public static void notes(N n) {
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+		N n = new N();
+		notes(n);
+		n.sumUp();
+	}
+}
