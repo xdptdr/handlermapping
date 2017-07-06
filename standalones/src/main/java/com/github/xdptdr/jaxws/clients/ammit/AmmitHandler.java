22e858352fc9a226f034407726c0d060f6fab5a6
xdptdr <xavierdpt@gmail.com>
2017-07-06 17:41:32 +0200

    .

--- standalones/src/main/java/com/github/xdptdr/jaxws/clients/ammit/AmmitHandler.java
+++ standalones/src/main/java/com/github/xdptdr/jaxws/clients/ammit/AmmitHandler.java
@@ -2,6 +2,8 @@ package com.github.xdptdr.jaxws.clients.ammit;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import javax.xml.namespace.QName;
@@ -12,9 +14,43 @@ import javax.xml.ws.handler.soap.SOAPMessageContext;
 
 public class AmmitHandler implements SOAPHandler<SOAPMessageContext> {
 
-	@SuppressWarnings({ "unused", "rawtypes" })
 	@Override
 	public boolean handleMessage(SOAPMessageContext context) {
+
+		for (Entry<String, Object> entry : context.entrySet()) {
+			StringBuffer buf = new StringBuffer();
+			buf.append(entry.getKey());
+			buf.append(" : ");
+			Object value = entry.getValue();
+			if (value != null) {
+				if (value instanceof String) {
+					buf.append(value);
+				} else if (value instanceof Number) {
+					buf.append(value);
+				} else if (value instanceof Boolean) {
+					buf.append(value);
+				} else if (value instanceof QName) {
+					buf.append(((QName) value).getPrefix() + " ; " + ((QName) value).getNamespaceURI() + " ; "
+							+ ((QName) value).getLocalPart());
+				} else if (value instanceof List) {
+					buf.append("[?]");
+				} else {
+					Class<?> c = value.getClass();
+					boolean sep = false;
+					while (c != Object.class) {
+						if (sep) {
+							buf.append(" ");
+						}
+						buf.append(c.getName());
+						c = c.getSuperclass();
+						sep = true;
+					}
+				}
+			}
+			System.out.println(buf.toString());
+
+		}
+
 		try {
 			context.getMessage().writeTo(System.out);
 		} catch (SOAPException | IOException e) {
