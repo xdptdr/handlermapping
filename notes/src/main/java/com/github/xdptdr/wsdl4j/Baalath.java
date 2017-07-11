ce156726bfd8fc2d86a8a5263f9f3560d3652e49
User <User@foobar>
2017-07-11 21:16:55 +0200

    .

--- notes/src/main/java/com/github/xdptdr/wsdl4j/Baalath.java
+++ notes/src/main/java/com/github/xdptdr/wsdl4j/Baalath.java
@@ -1,16 +1,49 @@
 package com.github.xdptdr.wsdl4j;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.wsdl.Definition;
+import javax.wsdl.WSDLException;
+import javax.wsdl.extensions.ExtensionRegistry;
 import javax.xml.namespace.QName;
 
+import com.ibm.wsdl.Constants;
+import com.ibm.wsdl.DefinitionImpl;
 import com.ibm.wsdl.extensions.http.HTTPAddressImpl;
+import com.ibm.wsdl.extensions.http.HTTPAddressSerializer;
 import com.ibm.wsdl.extensions.http.HTTPBindingImpl;
+import com.ibm.wsdl.extensions.http.HTTPConstants;
 import com.ibm.wsdl.extensions.http.HTTPOperationImpl;
 import com.ibm.wsdl.extensions.http.HTTPUrlEncodedImpl;
 import com.ibm.wsdl.extensions.http.HTTPUrlReplacementImpl;
 
 public class Baalath {
 
-	private static void foo() {
+	public static class StringWriterHolder {
+
+		private StringWriter stringWriter;
+		private PrintWriter printWriter;
+
+		public PrintWriter getNew() {
+			if (stringWriter == null) {
+				stringWriter = new StringWriter();
+				printWriter = new PrintWriter(stringWriter);
+			}
+			return printWriter;
+		}
+
+		public String consume() {
+			String str = stringWriter.toString();
+			printWriter.close();
+			stringWriter = null;
+			printWriter = null;
+			return str;
+		}
+
+	}
+
+	private static void fooImpl() {
 
 		QName elementType = null;
 		String locationURI = null;
@@ -70,13 +103,51 @@ public class Baalath {
 		elementType = urlReplacement.getElementType();
 		required = urlReplacement.getRequired();
 
-		
 		urlReplacement.toString();
 
 	}
 
-	public static void main(String[] args) {
-		foo();
+	private static void fooSer() throws WSDLException {
+
+		StringWriterHolder holder = new StringWriterHolder();
+
+		QName elementType = null;
+		ExtensionRegistry extensionRegistry = null;
+		Class<?> parentType = null;
+
+		Definition definition = new DefinitionImpl();
+		definition.addNamespace("http", HTTPConstants.NS_URI_HTTP);
+		definition.addNamespace("wsdl", Constants.NS_URI_WSDL);
+
+		HTTPAddressSerializer httpAddressSerializer = new HTTPAddressSerializer();
+		HTTPAddressImpl httpAddressImpl = new HTTPAddressImpl();
+
+		httpAddressSerializer.marshall(parentType, elementType, httpAddressImpl, holder.getNew(), definition,
+				extensionRegistry);
+		System.out.println(holder.consume());
+
+		httpAddressImpl.setRequired(false);
+
+		httpAddressSerializer.marshall(parentType, elementType, httpAddressImpl, holder.getNew(), definition,
+				extensionRegistry);
+		System.out.println(holder.consume());
+
+		httpAddressImpl.setLocationURI("titou");
+
+		httpAddressSerializer.marshall(parentType, elementType, httpAddressImpl, holder.getNew(), definition,
+				extensionRegistry);
+		System.out.println(holder.consume());
+
+		httpAddressImpl.setElementType(new QName("yipee", "yopla"));
+		
+		httpAddressSerializer.marshall(parentType, elementType, httpAddressImpl, holder.getNew(), definition,
+				extensionRegistry);
+		System.out.println(holder.consume());
+
+	}
+
+	public static void main(String[] args) throws WSDLException {
+		fooSer();
 	}
 
 }
