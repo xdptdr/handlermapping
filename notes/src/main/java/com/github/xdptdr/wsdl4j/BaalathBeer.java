e7f54cd04ed3189b293e560baa3cafbfccfc0742
User <User@foobar>
2017-07-12 07:46:48 +0200

    .

--- notes/src/main/java/com/github/xdptdr/wsdl4j/BaalathBeer.java
+++ notes/src/main/java/com/github/xdptdr/wsdl4j/BaalathBeer.java
@@ -1,10 +1,31 @@
 package com.github.xdptdr.wsdl4j;
 
+import com.ibm.wsdl.extensions.mime.MIMEConstants;
+import com.ibm.wsdl.extensions.mime.MIMEContentImpl;
+import com.ibm.wsdl.extensions.mime.MIMEContentSerializer;
+import com.ibm.wsdl.extensions.mime.MIMEMimeXmlImpl;
+import com.ibm.wsdl.extensions.mime.MIMEMimeXmlSerializer;
+import com.ibm.wsdl.extensions.mime.MIMEMultipartRelatedImpl;
+import com.ibm.wsdl.extensions.mime.MIMEMultipartRelatedSerializer;
+import com.ibm.wsdl.extensions.mime.MIMEPartImpl;
+
 public class BaalathBeer {
 	private static void foo() {
-		
+
+		MIMEConstants.class.getName();
+
+		MIMEContentSerializer mimeContentSerializer = new MIMEContentSerializer();
+		MIMEContentImpl mimeContentImpl = new MIMEContentImpl();
+
+		MIMEMimeXmlSerializer mimeMimeXmlSerializer = new MIMEMimeXmlSerializer();
+		MIMEMimeXmlImpl mimeMimeXmlImpl = new MIMEMimeXmlImpl();
+
+		MIMEMultipartRelatedSerializer mimeMultipartRelatedSerializer = new MIMEMultipartRelatedSerializer();
+		MIMEMultipartRelatedImpl mimeMultipartRelatedImpl = new MIMEMultipartRelatedImpl();
+
+		MIMEPartImpl mimePartImpl = new MIMEPartImpl();
 	}
-	
+
 	public static void main(String[] args) {
 		foo();
 	}
