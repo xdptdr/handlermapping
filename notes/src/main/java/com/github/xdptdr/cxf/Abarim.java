491c12c71fa5cee5fbf3f37b6214beba7a3d7585
User <User@foobar>
2017-07-17 22:06:13 +0200

    .

--- notes/src/main/java/com/github/xdptdr/cxf/Abarim.java
+++ notes/src/main/java/com/github/xdptdr/cxf/Abarim.java
@@ -5,8 +5,12 @@ import javax.jws.WebMethod;
 import org.apache.cxf.endpoint.Endpoint;
 import org.apache.cxf.endpoint.EndpointException;
 import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.frontend.WSDLGetInterceptor;
+import org.apache.cxf.interceptor.Interceptor;
 import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
 import org.apache.cxf.jaxws.JaxwsServiceBuilder;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.message.MessageImpl;
 import org.apache.cxf.service.model.EndpointInfo;
 import org.apache.cxf.service.model.ServiceInfo;
 import org.apache.cxf.wsdl.service.factory.ReflectionServiceFactoryBean;
@@ -20,6 +24,8 @@ public class Abarim {
 	private static Endpoint endpoint;
 	private static JaxWsServerFactoryBean jaxWsServerFactoryBean;
 	private static Server server;
+	private static WSDLGetInterceptor wsdlGetInterceptor;
+	private static Message message;
 
 	@WebMethod
 	public String foo(String in) {
@@ -39,9 +45,19 @@ public class Abarim {
 		jaxWsServerFactoryBean.setAddress("http://localhost:9000/Abarim");
 		jaxWsServerFactoryBean.setServiceBean(new Abarim());
 
-		server = jaxWsServerFactoryBean.create();
+		for (Interceptor<? extends Message> interceptor : endpoint.getInInterceptors()) {
+			if (interceptor instanceof WSDLGetInterceptor) {
+				wsdlGetInterceptor = (WSDLGetInterceptor) interceptor;
+			}
+		}
+
+		Message message = new MessageImpl();
+		wsdlGetInterceptor.handleMessage(message);
+
+		// server = jaxWsServerFactoryBean.create();
 
 		/*- wsdl?
+		    - org.apache.cxf.frontend.AbstractWSDLBasedEndpointFactory.createEndpoint()
 		    - org.apache.cxf.frontend.WSDLGetInterceptor.handleMessage(Message)
 		    - org.apache.cxf.frontend.WSDLGetUtils.writeWSDLDocument(Message, Map<String, Definition>, Map<String, SchemaReference>, String, String, EndpointInfo)
 		 */
