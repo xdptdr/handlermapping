7c89703abcfafb11c090bf61b127c0cd8bc61c0b
xdptdr <xavierdpt@gmail.com>
2017-07-17 18:54:47 +0200

    .

--- notes/src/main/java/com/github/xdptdr/cxf/Abarim.java
+++ notes/src/main/java/com/github/xdptdr/cxf/Abarim.java
@@ -2,24 +2,44 @@ package com.github.xdptdr.cxf;
 
 import javax.jws.WebMethod;
 
+import org.apache.cxf.endpoint.Endpoint;
+import org.apache.cxf.endpoint.EndpointException;
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
 import org.apache.cxf.jaxws.JaxwsServiceBuilder;
+import org.apache.cxf.service.model.EndpointInfo;
 import org.apache.cxf.service.model.ServiceInfo;
+import org.apache.cxf.wsdl.service.factory.ReflectionServiceFactoryBean;
 
 public class Abarim {
 
 	private static JaxwsServiceBuilder jaxwsServiceBuilder;
 	private static ServiceInfo serviceInfo;
+	private static ReflectionServiceFactoryBean reflectionServiceFactoryBean;
+	private static EndpointInfo endpointInfo;
+	private static Endpoint endpoint;
+	private static JaxWsServerFactoryBean jaxWsServerFactoryBean;
+	private static Server server;
 
 	@WebMethod
 	public String foo(String in) {
 		return in + "!" + in;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws EndpointException {
 		jaxwsServiceBuilder = new JaxwsServiceBuilder();
+		reflectionServiceFactoryBean = jaxwsServiceBuilder.getServiceFactory();
 		jaxwsServiceBuilder.setServiceClass(Abarim.class);
 		serviceInfo = jaxwsServiceBuilder.createService();
+		endpointInfo = reflectionServiceFactoryBean.getEndpointInfo();
+		endpoint = reflectionServiceFactoryBean.createEndpoint(endpointInfo);
+
+		jaxWsServerFactoryBean = new JaxWsServerFactoryBean();
+		jaxWsServerFactoryBean.setServiceClass(jaxwsServiceBuilder.getServiceClass());
+		jaxWsServerFactoryBean.setAddress("http://localhost:9000/Abarim");
+		jaxWsServerFactoryBean.setServiceBean(new Abarim());
 		
-		
+		 server = jaxWsServerFactoryBean.create();
+
 	}
 }
