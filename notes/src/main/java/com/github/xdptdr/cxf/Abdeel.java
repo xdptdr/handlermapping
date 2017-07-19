bda577c2ba0a0dd63cfd79f049b566b66044ea73
xdptdr <xavierdpt@gmail.com>
2017-07-19 16:08:26 +0200

    .

--- notes/src/main/java/com/github/xdptdr/cxf/Abdeel.java
+++ notes/src/main/java/com/github/xdptdr/cxf/Abdeel.java
@@ -1,21 +1,66 @@
 package com.github.xdptdr.cxf;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.zip.GZIPOutputStream;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.bus.extension.ExtensionManagerBus;
 import org.apache.cxf.bus.managers.PhaseManagerImpl;
 import org.apache.cxf.feature.Feature;
-import org.apache.cxf.helpers.HttpHeaderHelper;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.message.MessageImpl;
 import org.apache.cxf.phase.PhaseInterceptorChain;
 import org.apache.cxf.transport.common.gzip.GZIPFeature;
 
 public class Abdeel {
-	public static void main(String[] args) {
+
+	public static enum Path {
+
+		GET(true, false, false, false), GZIP_NOT_REQUESTOR(false, true, false, false), XGZIP_NOT_REQUESTOR(false, false,
+				true, false);
+
+		private boolean get;
+		private boolean gzip;
+		private boolean xgzip;
+		private boolean requestor;
+
+		Path(boolean isGet, boolean isGzip, boolean isXGzip, boolean isRequestor) {
+			this.get = isGet;
+			this.gzip = isGzip;
+			this.xgzip = isXGzip;
+			this.requestor = isRequestor;
+		}
+
+		public boolean isGet() {
+			return get;
+		}
+
+		public boolean isGzip() {
+			return gzip;
+		}
+
+		public boolean isXgzip() {
+			return xgzip;
+		}
+
+		public boolean isRequestor() {
+			return requestor;
+		}
+
+	};
+
+	public static void main(String[] args) throws IOException {
+
+		Path path = Path.GZIP_NOT_REQUESTOR;
+
 		Bus bus = BusFactory.getDefaultBus();
 		azzert(bus instanceof ExtensionManagerBus);
 
@@ -38,10 +83,27 @@ public class Abdeel {
 		pic.add(bus.getInInterceptors());
 
 		Message message = new MessageImpl();
-		Map<String, String> headers = new HashMap<>();
-		// message.put(Message.HTTP_REQUEST_METHOD, "GET");
-		message.put(Message.PROTOCOL_HEADERS, headers);
-		headers.put("content-encoding", "");
+		Map<String, List<String>> headers = new HashMap<>();
+		if (path == Path.GET) {
+			message.put(Message.HTTP_REQUEST_METHOD, "GET");
+		}
+		if (path == Path.GZIP_NOT_REQUESTOR || path == Path.XGZIP_NOT_REQUESTOR) {
+			message.put(Message.PROTOCOL_HEADERS, headers);
+			if (path == Path.GZIP_NOT_REQUESTOR) {
+				headers.put("content-encoding", Arrays.asList(new String[] { "gzip" }));
+
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				GZIPOutputStream gout = new GZIPOutputStream(baos);
+				gout.write("hello".getBytes());
+				InputStream is = new ByteArrayInputStream(baos.toByteArray());
+
+				message.setContent(InputStream.class, is);
+
+				message.put(Message.REQUESTOR_ROLE, true);
+			} else if (path == Path.XGZIP_NOT_REQUESTOR) {
+				headers.put("content-encoding", Arrays.asList(new String[] { "xgzip" }));
+			}
+		}
 		pic.doIntercept(message);
 
 		// BeanValidationFeature.class.getName();
