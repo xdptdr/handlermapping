7b87eeb0e61ea8e425fd523ed45e6e0bcf7a6adf
xdptdr <xavierdpt@gmail.com>
2017-07-19 14:35:05 +0200

    .

--- notes/src/main/java/com/github/xdptdr/cxf/Abdeel.java
+++ notes/src/main/java/com/github/xdptdr/cxf/Abdeel.java
@@ -1,25 +1,48 @@
 package com.github.xdptdr.cxf;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.cxf.Bus;
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.bus.extension.ExtensionManagerBus;
+import org.apache.cxf.bus.managers.PhaseManagerImpl;
 import org.apache.cxf.feature.Feature;
+import org.apache.cxf.helpers.HttpHeaderHelper;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.message.MessageImpl;
+import org.apache.cxf.phase.PhaseInterceptorChain;
 import org.apache.cxf.transport.common.gzip.GZIPFeature;
 
 public class Abdeel {
 	public static void main(String[] args) {
 		Bus bus = BusFactory.getDefaultBus();
 		azzert(bus instanceof ExtensionManagerBus);
+
+		bus.getInInterceptors().clear();
+		bus.getInFaultInterceptors().clear();
+		bus.getOutInterceptors().clear();
+		bus.getOutFaultInterceptors().clear();
+
 		azzert(bus.getFeatures().size() == 0);
 
 		GZIPFeature gzipFeature = new GZIPFeature();
-		gzipFeature.setForce(true);
-		bus.getFeatures().add(gzipFeature);
-		gzipFeature.initialize(bus);
 		azzert(gzipFeature instanceof Feature);
-		azzert(bus.getFeatures().size() == 1);
-		azzert(bus.getFeatures().iterator().next() == gzipFeature);
 
+		gzipFeature.setForce(true);
+		gzipFeature.initialize(bus);
+
+		PhaseManagerImpl pmi = new PhaseManagerImpl();
+
+		PhaseInterceptorChain pic = new PhaseInterceptorChain(pmi.getInPhases());
+		pic.add(bus.getInInterceptors());
+
+		Message message = new MessageImpl();
+		Map<String, String> headers = new HashMap<>();
+		// message.put(Message.HTTP_REQUEST_METHOD, "GET");
+		message.put(Message.PROTOCOL_HEADERS, headers);
+		headers.put("content-encoding", "");
+		pic.doIntercept(message);
 
 		// BeanValidationFeature.class.getName();
 		// CertConstraintsFeature.class.getName();
