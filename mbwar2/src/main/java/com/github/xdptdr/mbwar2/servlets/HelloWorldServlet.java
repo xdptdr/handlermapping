e1dc882b7d1c6128235d1558e11be36bb6168ccf
xdptdr <xavierdpt@gmail.com>
2017-08-01 13:57:36 +0200

    .

--- mbwar2/src/main/java/com/github/xdptdr/mbwar2/servlets/HelloWorldServlet.java
+++ mbwar2/src/main/java/com/github/xdptdr/mbwar2/servlets/HelloWorldServlet.java
@@ -12,6 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloWorldServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+	
+	// jbossws-cxf-server : org.jboss.wsf.stack.cxf.metadata.MetadataBuilder.createDDEndpoint(Class<?>, ArchiveDeployment, Endpoint)
+	
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
